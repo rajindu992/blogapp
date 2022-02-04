6d5044d3ba87fe20860d84e9674a018cfb013342
rajindu <rajindu992@gmail.com>
2022-02-04 14:25:54 +0530

    final commit

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 
-from blogproject.authapp import views
+from authapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,5 +31,6 @@ urlpatterns = [
     path('admins/',include('blogadmin.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, }),
 
 ]+  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

03f9d8f3830aa763afa7ea310e0eb7db0da64289
rajindu <rajindu992@gmail.com>
2022-02-07 10:45:08 +0530

    changed settings to implement static

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join( BASE_DIR,'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -146,6 +143,6 @@ LOGIN_REDIRECT_URL='loghome'
 LOGOUT_REDIRECT_URL ='loghome'
 
 
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 django_heroku.settings(locals())

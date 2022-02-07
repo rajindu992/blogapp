5f39bcd1509d721ed9948117147b05e05519e7ff
rajindu <rajindu992@gmail.com>
2022-02-07 11:12:09 +0530

    changing static file

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -33,5 +33,4 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

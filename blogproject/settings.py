d8641f72fac0c7b34e439720391959eac3ee6ffd
rajindu <rajindu992@gmail.com>
2022-02-17 13:44:52 +0530

    changing the media files

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -107,7 +107,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
